9b1eda7c7a59017e53d90dfffd29de924ae835e4
张建勇 <zhangjianyongke@gmail.com>
2019-08-18 20:00:19 +0800

    vue 基础学习h5方式和vue cli方式

---
+++ vue-cli/todolist/src/TodoList.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+      <div>
+            <input class="item" v-model="inputValue"/>
+            <button @click="handleSumbit">提交</button>
+      </div>
+      <ul>
+        <todo-item 
+        v-for="(item , index) of list"
+        :key="index"
+        :content="item"
+        :index ="index"
+        @delete="handelDelete"></todo-item>
+      </ul>
+  </div>
+
+</template>
+
+<script>
+import TodoItem from './components/TodoItem'
+
+export default {
+  components : {
+    'todo-item' : TodoItem
+  },
+  data (){
+    return {
+      inputValue :'',
+      list :[]
+    }
+  },
+  methods :{
+    handleSumbit (){
+      this.list.push(this.inputValue)
+      this.inputValue = ''
+    },
+    handelDelete (index){
+      this.list.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+  .item{
+    color: #000;
+  }
+</style>
---
+++ vue-cli/todolist/src/components/TodoItem.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="item" @click="handelDelete">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props :['content','index'],
+  methods :{
+    handelDelete (){
+      this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+
+<!-- 添加“scoped”属性以仅将CSS限制到此组件 -->
+<style scoped>
+  .item{
+    color: red;
+  }
+</style>
